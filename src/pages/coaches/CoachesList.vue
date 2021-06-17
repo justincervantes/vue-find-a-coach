1b6cd52cc6717fa3dfd0d60ad3be4d1b3a296342
Justin Cervantes <justin.cervantes@telus.com>
2021-06-17 13:51:00 -0600

    Adding error handling

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,4 +1,7 @@
 <template>
+  <base-dialog :show="!!error" title="An error occured" @close="handleError">
+    <p>{{ error }}</p>
+  </base-dialog>
   <section>
     <coach-filter @change-filter="setFilters"></coach-filter>
   </section>
@@ -45,7 +48,8 @@ export default {
         backend: true,
         career: true
       },
-      isLoading: false
+      isLoading: false,
+      error: null
     };
   },
   components: {
@@ -81,8 +85,15 @@ export default {
     },
     async loadCoaches() {
       this.isLoading = true;
-      await this.$store.dispatch('coaches/loadCoaches');
+      try {
+        await this.$store.dispatch('coaches/loadCoaches');
+      } catch (error) {
+        this.error = error.message || 'Something went wrong';
+      }
       this.isLoading = false;
+    },
+    handleError() {
+      this.error = null;
     }
   }
 };
