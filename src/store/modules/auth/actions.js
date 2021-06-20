913aaa41136a508917443dfb3427a766a882e902
Justin Cervantes <justin.cervantes@telus.com>
2021-06-20 17:20:09 -0600

    Wrapping up authentication with AutoLogouts

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,17 +1,19 @@
+let timer;
+
 export default {
-  //async auth(context, payload) {},
-  async login(context, payload) {
-    const response = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBMwjl1l60WQsakjkbBWuF2-xpLVGZNkZM',
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true
-        })
-      }
-    );
+  async auth(context, payload) {
+    const url =
+      payload.mode === 'login'
+        ? 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBMwjl1l60WQsakjkbBWuF2-xpLVGZNkZM'
+        : 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBMwjl1l60WQsakjkbBWuF2-xpLVGZNkZM';
+    const response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true
+      })
+    });
 
     const responseData = await response.json();
 
@@ -23,48 +25,78 @@ export default {
       throw error;
     }
     console.log(responseData);
+
+    const expiresIn = +responseData.expiresIn * 1000;
+    // const expiresIn = 5000; // testing
+
+    const expirationDate = new Date().getTime() + expiresIn;
+
+    localStorage.setItem('token', responseData.idToken);
+    localStorage.setItem('userId', responseData.localId);
+    localStorage.setItem('tokenExpiration', expirationDate);
+
+    timer = setTimeout(function() {
+      context.dispatch('logout');
+      context.dispatch('autoLogout');
+    }, expiresIn);
+
     context.commit('setUser', {
       token: responseData.idToken,
       userId: responseData.localId,
       tokenExpiration: responseData.expiresIn
+    });
+  },
+  tryLogin(context) {
+    const token = localStorage.getItem('token');
+    const userId = localStorage.getItem('userId');
+    const tokenExpiration = localStorage.getItem('tokenExpiration');
+
+    const expiresIn = +tokenExpiration - new Date().getTime();
+
+    if (expiresIn < 0) {
+      return;
+    }
+
+    timer = setTimeout(function() {
+      context.dispatch('autoLogout');
+    }, expiresIn);
+
+    if (token && userId) {
+      context.commit('setUser', {
+        token: token,
+        userId: userId,
+        tokenExpiration: null
+      });
+    }
+  },
+  async login(context, payload) {
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'login'
     });
   },
 
   async signup(context, payload) {
-    const response = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBMwjl1l60WQsakjkbBWuF2-xpLVGZNkZM',
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true
-        })
-      }
-    );
-
-    const responseData = await response.json();
-
-    if (!response.ok) {
-      console.log(responseData);
-      const error = new Error(
-        responseData.message || 'Failed to authenticate. Check your login data.'
-      );
-      throw error;
-    }
-    console.log(responseData);
-    context.commit('setUser', {
-      token: responseData.idToken,
-      userId: responseData.localId,
-      tokenExpiration: responseData.expiresIn
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'signup'
     });
   },
 
   logout(context) {
+    clearTimeout(timer);
+
+    localStorage.removeItem('token');
+    localStorage.removeItem('userId');
+    localStorage.removeItem('tokenExpiration');
+
     context.commit('setUser', {
       token: null,
-      stateuserId: null,
-      tokenExpiration: null
+      stateuserId: null
     });
+  },
+  autoLogout(context) {
+    context.dispatch('logout');
+    context.commit('setAutoLogout');
   }
 };
